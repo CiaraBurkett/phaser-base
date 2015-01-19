59922474cfa5b3abda69cdb98c9a5aaaf6a68815
Ciara Burkett <ciaraburkett@gmail.com>
2015-01-19 09:54:42 -0500

    Add basic game skeleton

--- js/game.js
+++ js/game.js
@@ -1,8 +1,16 @@
-var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'game');
+var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'game', {preload: preload, create: create, update: update, render: render});
 
-game.state.add('boot', boot);
-game.state.add('load', load);
-game.state.add('menu', menu);
-game.state.add('play', play);
 
-game.state.start('boot');
+var player, cursor, map, layer;
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
+
+function render() {
+}
